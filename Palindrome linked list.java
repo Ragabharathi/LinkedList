04a0770c27014ab176be184e7bd448b5e6e9b9ef
RAGABHARATHI A <84565134+Ragabharathi@users.noreply.github.com>
2022-01-21 07:40:38 +0530

    Create Palindrome linked list.java

---
+++ Palindrome linked list.java
@@ -0,0 +1,54 @@
+Given the head of a singly linked list, return true if it is a palindrome.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+
+
+
+
+
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast != null)
+            slow = slow.next;
+        
+        slow = reverse(slow);
+        fast = head;
+        
+        while(slow != null){
+            if(slow.val != fast.val)
+                return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    
+    private ListNode reverse(ListNode head){
+        ListNode newHead = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
